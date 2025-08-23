c9ea73cedeec9c3f0fe77ef6b09e6a0d9cd856e6
DropDeart <97798580+DropDeart@users.noreply.github.com>
2025-08-23 14:46:18 +0300

    Delete frontend/kayra-export-frontend directory

--- frontend/kayra-export-frontend/next.config.ts
+++
@@ -1,8 +0,0 @@
-import path from "path";
-import { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(process.cwd(), "../../"),
-};
-
-export default nextConfig;
